c8f173ef1c0b94cbf60ea29ffeb34dd97be4d76b
ashish2508 <jha250805@gmail.com>
2025-05-19 21:19:17 +0530

    dark bg

--- lua/PurpleILike/colors.lua
+++ lua/PurpleILike/colors.lua
@@ -1,5 +1,5 @@
-local config=require("PurpleILike.config")
-local hsl=require("PurpleILike.utils").hsl
+local config=require("vercel.config")
+local hsl=require("vercel.utils").hsl
 
 local M={
 standardWhite="#ffffff",
@@ -7,49 +7,49 @@ standardBlack="#000000",
 }
 
 if vim.o.background=="light" then
-M.editorBackground=config.transparent and "NONE" or hsl(0,0,100)
-M.popupBackground=hsl(260,24,98)
-M.menuOptionBackground=hsl(260,18,92)
+M.editorBackground=config.transparent and "NONE" or "#ffffff"
+M.popupBackground=hsl(260,10,97)
+M.menuOptionBackground=hsl(260,8,92)
 
-M.mainText=hsl(258,30,15)
+M.mainText=hsl(260,25,15)
 M.inactiveText=hsl(260,10,50)
-M.lineNumberText=hsl(260,20,45)
-M.lineNumberTextActive=hsl(260,40,35)
-M.selectedText=hsl(258,25,90)
-M.border=hsl(260,18,88)
+M.lineNumberText=hsl(260,12,45)
+M.lineNumberTextActive=hsl(260,25,35)
+M.selectedText=hsl(260,20,90)
+M.border=hsl(260,8,85)
 
-M.syntaxConstant=hsl(265,80,40)
-M.syntaxKeyword=hsl(280,70,35)
-M.syntaxFunction=hsl(250,70,35)
-M.syntaxProperty=hsl(258,30,15)
+M.syntaxConstant=hsl(265,60,40)
+M.syntaxKeyword=hsl(280,55,38)
+M.syntaxFunction=hsl(250,55,38)
+M.syntaxProperty=hsl(260,25,15)
 
-M.errorText=hsl(340,70,40)
+M.errorText=hsl(0,70,42)
 M.warningText=hsl(30,70,45)
 M.commentText=hsl(260,10,50)
-M.stringText=hsl(150,40,35)
-M.successText=hsl(142,60,35)
+M.stringText=hsl(150,35,40)
+M.successText=hsl(142,55,38)
 else
-M.editorBackground=config.transparent and "NONE" or hsl(258,20,6)
-M.popupBackground=hsl(258,15,11)
-M.menuOptionBackground=hsl(258,15,13)
+M.editorBackground=config.transparent and "NONE" or "#000000"
+M.popupBackground=hsl(260,6,12)
+M.menuOptionBackground=hsl(260,6,14)
 
-M.mainText=hsl(258,40,90)
-M.inactiveText=hsl(260,15,60)
-M.lineNumberText=hsl(260,20,40)
-M.lineNumberTextActive=hsl(260,25,80)
-M.selectedText=hsl(258,35,25)
-M.border=hsl(258,15,20)
+M.mainText=hsl(260,30,88)
+M.inactiveText=hsl(260,12,60)
+M.lineNumberText=hsl(260,14,40)
+M.lineNumberTextActive=hsl(260,20,75)
+M.selectedText=hsl(260,25,25)
+M.border=hsl(260,6,18)
 
-M.syntaxConstant=hsl(265,80,70)
-M.syntaxKeyword=hsl(280,80,75)
-M.syntaxFunction=hsl(250,80,75)
-M.syntaxProperty=hsl(258,40,90)
+M.syntaxConstant=hsl(265,70,70)
+M.syntaxKeyword=hsl(280,70,75)
+M.syntaxFunction=hsl(250,70,75)
+M.syntaxProperty=hsl(260,30,88)
 
-M.errorText=hsl(350,70,60)
-M.warningText=hsl(30,70,70)
-M.commentText=hsl(260,15,60)
-M.stringText=hsl(145,40,65)
-M.successText=hsl(142,60,50)
+M.errorText=hsl(0,65,60)
+M.warningText=hsl(30,65,70)
+M.commentText=hsl(260,12,60)
+M.stringText=hsl(145,35,60)
+M.successText=hsl(142,55,50)
 end
 
 return M
