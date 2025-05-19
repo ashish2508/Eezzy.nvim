5965299e05029304f81238a1acc8eb229ee3cead
ashish2508 <jha250805@gmail.com>
2025-05-19 21:04:49 +0530

    palette

--- lua/vercel/colors.lua
+++ lua/vercel/colors.lua
@@ -1,55 +1,56 @@
-local config = require("vercel.config")
-local hsl = require("vercel.utils").hsl
+local config=require("vercel.config")
+local hsl=require("vercel.utils").hsl
 
-local M = {
-	standardWhite = "#ffffff",
-	standardBlack = "#000000",
+local M={
+standardWhite="#ffffff",
+standardBlack="#000000",
 }
 
-if vim.o.background == "light" then
-	M.editorBackground = config.transparent and "NONE" or hsl(1, 9, 2)
-	M.popupBackground = hsl(285, 10, 100)
-	M.menuOptionBackground = hsl(281, 0, 78)
+if vim.o.background=="light" then
+M.editorBackground=config.transparent and "NONE" or hsl(258,20,95)
+M.popupBackground=hsl(260,24,98)
+M.menuOptionBackground=hsl(260,18,92)
 
-	M.mainText = hsl(280, 10, 4)
-	M.inactiveText = hsl(0, 3, 81)
-	M.lineNumberText = hsl(281, 13, 9)
-	M.lineNumberTextActive = hsl(281, 13, 67)
-	M.selectedText = hsl(280, 0, 83)
-	M.border = hsl(280, 30, 98)
+M.mainText=hsl(258,30,15)
+M.inactiveText=hsl(260,10,50)
+M.lineNumberText=hsl(260,20,45)
+M.lineNumberTextActive=hsl(260,40,35)
+M.selectedText=hsl(258,25,90)
+M.border=hsl(260,18,88)
 
-	M.syntaxConstant = hsl(211, 100, 42)
-	M.syntaxKeyword = hsl(336, 65, 45)
-	M.syntaxFunction = hsl(274, 71, 43)
-	M.syntaxProperty = hsl(0, 0, 4)
+M.syntaxConstant=hsl(265,80,40)
+M.syntaxKeyword=hsl(280,70,35)
+M.syntaxFunction=hsl(250,70,35)
+M.syntaxProperty=hsl(258,30,15)
 
-	M.errorText = hsl(3, 62, 42)
-	M.warningText = hsl(24, 79, 43)
-	M.commentText = hsl(0, 0, 40)
-	M.stringText = hsl(139, 79, 28)
-	M.successText = hsl(142.1, 76.2, 36.3)
+M.errorText=hsl(340,70,40)
+M.warningText=hsl(30,70,45)
+M.commentText=hsl(260,10,50)
+M.stringText=hsl(150,40,35)
+M.successText=hsl(142,60,35)
 else
-	M.editorBackground = config.transparent and "NONE" or hsl(240, 10, 3.9)
-	M.popupBackground = hsl(240, 3.7, 15.9)
-	M.menuOptionBackground = hsl(0, 0, 17)
+M.editorBackground=config.transparent and "NONE" or hsl(258,20,6)
+M.popupBackground=hsl(258,15,11)
+M.menuOptionBackground=hsl(258,15,13)
 
-	M.mainText = hsl(0, 0, 93)
-	M.inactiveText = hsl(0, 0, 64)
-	M.lineNumberText = hsl(0, 0, 30)
-	M.lineNumberTextActive = hsl(0, 0, 95)
-	M.selectedText = hsl(0, 0, 93)
-	M.border = hsl(0, 0, 13)
+M.mainText=hsl(258,40,90)
+M.inactiveText=hsl(260,15,60)
+M.lineNumberText=hsl(260,20,40)
+M.lineNumberTextActive=hsl(260,25,80)
+M.selectedText=hsl(258,35,25)
+M.border=hsl(258,15,20)
 
-	M.syntaxConstant = hsl(210, 100, 66)
-	M.syntaxKeyword = hsl(341, 90, 67)
-	M.syntaxFunction = hsl(275, 80, 71)
-	M.syntaxProperty = hsl(0, 0, 93)
+M.syntaxConstant=hsl(265,80,70)
+M.syntaxKeyword=hsl(280,80,75)
+M.syntaxFunction=hsl(250,80,75)
+M.syntaxProperty=hsl(258,40,90)
 
-	M.errorText = hsl(5, 61, 54)
-	M.warningText = hsl(40, 71, 73)
-	M.commentText = hsl(0, 0, 64)
-	M.stringText = hsl(131, 43, 57)
-	M.successText = hsl(142.1, 76.2, 36.3)
+M.errorText=hsl(350,70,60)
+M.warningText=hsl(30,70,70)
+M.commentText=hsl(260,15,60)
+M.stringText=hsl(145,40,65)
+M.successText=hsl(142,60,50)
 end
 
 return M
+
