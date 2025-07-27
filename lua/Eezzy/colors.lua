7b17e1b5d702bafbe53f64bb88f3213f0437c766
ashish2508 <jha250805@gmail.com>
2025-07-27 20:07:17 +0530

    bg

--- lua/Eezzy/colors.lua
+++ lua/Eezzy/colors.lua
@@ -1,36 +1,36 @@
-local config=require("Eezzy.config")
-local hsl=require("Eezzy.utils").hsl
+local config            = require("Eezzy.config")
+local hsl               = require("Eezzy.utils").hsl
 
-local M={
-  standardBlack="#000000", 
+local M                 = {
+  standardBlack = "#000000",
 }
 
-M.editorBackground       = config.transparent and "NONE" or "#101010"
-M.currentLineBackground  = "#1e2424"
-M.selectionBackground    = "#3c4848"
-M.popupBackground        = "#141818"
-M.menuOptionBackground   = "#1e2424"
-M.border                 = "#1e2424"
-M.mutedForeground        = "#909090"
-M.mainText               = "#d8d8d8"
-M.selectedText           = "#f8f8f8"
-M.inactiveText           = "#909090"
-M.lineNumberText         = "#3c4848"
-M.lineNumberTextActive   = "#909090"
-M.commentText            = "#909090"
+M.editorBackground      = config.transparent and "NONE" or "#27272a"
+M.currentLineBackground = "#1e2424"
+M.selectionBackground   = "#3c4848"
+M.popupBackground       = "#141818"
+M.menuOptionBackground  = "#1e2424"
+M.border                = "#1e2424"
+M.mutedForeground       = "#909090"
+M.mainText              = "#d8d8d8"
+M.selectedText          = "#f8f8f8"
+M.inactiveText          = "#909090"
+M.lineNumberText        = "#3c4848"
+M.lineNumberTextActive  = "#909090"
+M.commentText           = "#909090"
 
-M.syntaxConstant         = "#6080ff"
-M.syntaxKeyword          = "#ff0e6b"
-M.syntaxFunction         = "#40c4aa"
-M.syntaxProperty         = "#48fb9f"
+M.syntaxConstant        = "#6080ff"
+M.syntaxKeyword         = "#ff0e6b"
+M.syntaxFunction        = "#40c4aa"
+M.syntaxProperty        = "#48fb9f"
 
-M.stringText             = "#8bbb81"
-M.syntaxNumber           = "#c080ff"
-M.syntaxType             = "#ff9020"
-M.syntaxClass            = "#a0ff20"
+M.stringText            = "#8bbb81"
+M.syntaxNumber          = "#c080ff"
+M.syntaxType            = "#ff9020"
+M.syntaxClass           = "#a0ff20"
 
-M.errorText              = "#e54c4c"
-M.warningText            = "#f2be59"
-M.successText            = "#54aa46"
+M.errorText             = "#e54c4c"
+M.warningText           = "#f2be59"
+M.successText           = "#54aa46"
 
 return M
