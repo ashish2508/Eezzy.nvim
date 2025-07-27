18ff02aa889e9adbf570c6e5440d1753d12f1ed9
ashish2508 <jha250805@gmail.com>
2025-07-27 20:08:27 +0530

    bg

--- lua/Eezzy/colors.lua
+++ lua/Eezzy/colors.lua
@@ -5,7 +5,7 @@ local M                 = {
   standardBlack = "#000000",
 }
 
-M.editorBackground      = config.transparent and "NONE" or "#27272a"
+M.editorBackground      = config.transparent and "NONE" or "#3f3f46"
 M.currentLineBackground = "#1e2424"
 M.selectionBackground   = "#3c4848"
 M.popupBackground       = "#141818"
